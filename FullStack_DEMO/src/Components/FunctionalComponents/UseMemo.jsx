95ac3ab6c7b72a8dbc165a8bc03fed63eb13081b
Anas <anashenrya@gmail.com>
2025-01-02 14:29:42 +0530

    Seventh Commit

--- FullStack_DEMO/src/Components/FunctionalComponents/UseMemo.jsx
+++ FullStack_DEMO/src/Components/FunctionalComponents/UseMemo.jsx
@@ -1,10 +1,36 @@
-import React from "react";
-const UseMeme = () =>{
-    return(
-        <div>
-            <h1>This is useMemo Example</h1>
-            <p>It can be used to keep expensive, resource intensive functions from needlessly running.</p>
-        </div>
-    )
-}
+import React, { useMemo, useState } from "react";
+var slowfn = (num) => {
+  for (let i = 0; i < 3000000000; i++) {}
+  return num * 2;
+};
+const UseMeme = () => {
+  var [num, setNum] = useState(0);
+  var [theme, updateTheme] = useState(true);
+  var starNight = {
+    backgroundColor: theme ? "black" : "white",
+    color: theme ? "white" : "black",
+  };
+  var doublingAnum = useMemo(() => {
+    return slowfn(num);
+  }, [num]);
+  return (
+    <div>
+      <h1>This is useMemo Example</h1>
+      <p>
+        It can be used to keep expensive, resource intensive functions from
+        needlessly running. When using memo, the place where it is used mustn't
+        be a function
+      </p>
+      <input
+        type='number'
+        value={num}
+        onChange={(event) => setNum(event.target.value)}
+      />
+      <button onClick={() => updateTheme((theme = !theme))}>
+        Toggle Theme
+      </button>
+      <h2 style={starNight}>{doublingAnum}</h2>
+    </div>
+  );
+};
 export default UseMeme;
