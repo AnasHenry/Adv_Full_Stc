02ece4793f557d50307de23fbb9d6280c57a0bf2
Anas <anashenrya@gmail.com>
2025-01-06 09:27:00 +0530

    Modifications made on 9th Commit

--- FullStack_DEMO/src/Components/FunctionalComponents/Landing.jsx
+++ FullStack_DEMO/src/Components/FunctionalComponents/Landing.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 var Land = () =>{
+    var navigate = useNavigate();
     let username = localStorage.getItem('user');
+    var logoutfn = () =>{
+        localStorage.clear();
+        navigate("/login");
+    }
     return(
-        <div>
+        <div className="flex">
             <h1>Welcome, {username}</h1>
+            <button onClick={logoutfn} style={{height:40}}>Log Out</button>
         </div>
     )
 }
